e32b799d01479f089f7c4afaa66b29b2e2178f7a
Víctor Mazo <71872313+viamazoba@users.noreply.github.com>
2023-08-25 16:54:27 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,37 @@ const app = express();
 app.use(express.json());
 
 const port = 3000;
+let newTasks = [... tasks]
 
-app.get('/api', (_, res: Response) => {
-    res.send('Hello World!');
+app.get('/api/tasks', (_, res: Response) => {
+    res.send(newTasks);
+});
+
+app.get('/api/tasks/:id', (req: Request, res: Response) => {
+    const {id} = req.params
+    const chooseTask = newTasks.filter((task)=>{
+        return task.id.toString() === id
+    })
+    res.send(chooseTask);
+});
+
+app.delete('/api/tasks/:id', (req: Request, res: Response) => {
+    const {id} = req.params
+    const chooseTasks = newTasks.filter((task)=>{
+        return task.id.toString() != id
+    })
+
+    newTasks = [... chooseTasks]
+    res.send(newTasks);
+});
+
+app.post('/api/tasks', (req:Request, res: Response) => {
+    const {task} = req.body
+    newTasks = [
+        ...tasks,
+        task
+    ]
+    res.send(newTasks);
 });
 
 
